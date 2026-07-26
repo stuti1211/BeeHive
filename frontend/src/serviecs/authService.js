d8621f10c3e2c9d29a8cb5cb99db625c185d5bcf
Swriel <multipro.geneigo@gmail.com>
2026-07-26 18:22:55 +0530

    frontend authServices changes

--- frontend/src/serviecs/authService.js
+++ frontend/src/serviecs/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //import { response } from '../../../backend/src/app';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const signup = async (userData) => {
     const response = await axios.post(
@@ -38,7 +38,7 @@ export const getFiles = async () => {
     const token = localStorage.getItem('token');
 
     const response = await axios.get(
-        'http://localhost:5000/api/files',
+         `${API_URL}/api/files`,
         {
             headers: {
                 Authorization: `Bearer ${token}`
@@ -53,7 +53,7 @@ export const uploadFile = async (file) => {
     const formData = new FormData();
     formData.append('file', file);
     const response = await axios.post(
-        'http://localhost:5000/api/files/upload',
+         `${API_URL}api/files/upload`,
         formData,
         {
             headers: {
@@ -68,7 +68,7 @@ export const uploadFile = async (file) => {
  export const viewFiles = async(id)=> {
     const token = localStorage.getItem("token");
     const response = await axios.get(
-       `http://localhost:5000/api/files/${id}/view`,
+        `${API_URL}/api/files/${id}/view`,
       {
          headers: {
                 Authorization: `Bearer ${token}`,
@@ -84,7 +84,7 @@ export const uploadFile = async (file) => {
  export const deleteFiles= async(id)=>{
     const token =localStorage.getItem("token");
     const response =await axios.delete(
-        `http://localhost:5000/api/files/${id}/delete`,
+         `${API_URL}/api/files/${id}/delete`,
         {
          headers: {
                 Authorization: `Bearer ${token}`,
